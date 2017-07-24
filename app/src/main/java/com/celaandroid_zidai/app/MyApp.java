9aad6300a95debefd323e027920019bb9e364114
ColdRainyNight <heimanba08@.com>
2017-07-24 21:13:58 +0800

    一些文件

--- app/src/main/java/com/celaandroid_zidai/app/MyApp.java
+++ app/src/main/java/com/celaandroid_zidai/app/MyApp.java
@@ -2,6 +2,10 @@ package com.celaandroid_zidai.app;
 
 import android.app.Application;
 
+import com.celaandroid_zidai.User;
+import com.umeng.socialize.PlatformConfig;
+import com.umeng.socialize.UMShareAPI;
+
 import org.xutils.BuildConfig;
 import org.xutils.x;
 
@@ -11,10 +15,40 @@ import org.xutils.x;
  * 创建时间：2017/7/19 9:13
  */
 public class MyApp extends Application {
+    {
+        PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
+    }
+
+    private static MyApp sApp;
+    private User user;
+    private UMShareAPI mUMShareAPI;
+
     @Override
     public void onCreate() {
         super.onCreate();
         x.Ext.init(this);
         x.Ext.setDebug(BuildConfig.DEBUG);
+        sApp = this;
+        mUMShareAPI = UMShareAPI.get(this);
+    }
+
+    public static MyApp getInstance() {
+        return sApp;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public UMShareAPI getUMShareAPI() {
+        return mUMShareAPI;
+    }
+
+    public void setUMShareAPI(UMShareAPI UMShareAPI) {
+        mUMShareAPI = UMShareAPI;
     }
 }
